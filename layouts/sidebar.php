8a2e1219e267ec7078e6751356ae0efedc407429
Honey Lou L. Danila <tradebryant@gmail.com>
2023-11-20 09:31:06 +0800

    awit

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -55,15 +55,48 @@ $pend = $pending[0]["count"];
           <?php else: ?>
             <li><a href="schedule"><i class="fa fa-calendar"></i> <span>Schedule</span></a></li>
           <?php endif; ?>
-          <li><a href="transfer?action=list"><i class="fa fa-bell"></i> <span>Notice to Transfer</span>
-            <span class="pull-right-container">
-              <span class="label label-danger pull-right">5</span>
-            </span></a></li>
 
-            <li><a href="resources/pdf_installer.exe"><i class="fa fa-bell"></i> <span>PDF Installer</span>
+
+          <?php
+          
+          $count = query("SELECT COUNT(*) AS count
+          FROM (
+              SELECT
+                  n.profile_id,
+                  MAX(CASE WHEN n.year_date <= CURDATE() AND (ns.year_notif_status = '' OR ns.year_notif_status IS NULL) THEN 1 ELSE 0 END) AS year_need_to_notify,
+                  MAX(CASE WHEN n.6months_date <= CURDATE() AND (ns.6months_notif_status = '' OR ns.6months_notif_status IS NULL) THEN 1 ELSE 0 END) AS six_months_need_to_notify,
+                  MAX(CASE WHEN n.3months_date <= CURDATE() AND (ns.3months_notif_status = '' OR ns.3months_notif_status IS NULL) THEN 1 ELSE 0 END) AS three_months_need_to_notify,
+                  MAX(CASE WHEN n.month_date <= CURDATE() AND (ns.month_notif_status = '' OR ns.month_notif_status IS NULL) THEN 1 ELSE 0 END) AS month_need_to_notify,
+                  MAX(CASE WHEN n.week_date <= CURDATE() AND (ns.week_notif_status = '' OR ns.week_notif_status IS NULL) THEN 1 ELSE 0 END) AS week_need_to_notify,
+                  MAX(CASE WHEN n.day_before_date <= CURDATE() AND (ns.day_before_notif_status = '' OR ns.day_before_notif_status IS NULL) THEN 1 ELSE 0 END) AS day_before_need_to_notify,
+                  MAX(CASE WHEN n.date_expired <= CURDATE() AND (ns.day_notif_status = '' OR ns.day_notif_status IS NULL) THEN 1 ELSE 0 END) AS date_expired_need_to_notify
+              FROM notification n
+              LEFT JOIN notification_status ns ON n.notification_id = ns.notification_id
+              LEFT JOIN profile_list p ON p.profile_id = n.profile_id
+              WHERE p.active_status IS NULL OR p.active_status != 'FORMER'
+              GROUP BY n.profile_id
+          ) AS subquery
+          WHERE
+              year_need_to_notify != 0 OR
+              six_months_need_to_notify != 0 OR
+              three_months_need_to_notify != 0 OR
+              month_need_to_notify != 0 OR
+              week_need_to_notify != 0 OR
+              day_before_need_to_notify != 0 OR
+              date_expired_need_to_notify != 0;");
+          
+          ?>
+
+          <li><a href="transfer?action=list"><i class="fa fa-bell"></i> <span>Notice to Transfer</span>
+          <?php if($count[0]["count"] != 0): ?>
             <span class="pull-right-container">
-              <span class="label label-danger pull-right">5</span>
-            </span></a></li>
+              <span class="label label-danger pull-right"><?php echo($count[0]["count"]); ?></span>
+            </span>
+          <?php endif; ?>
+          </a></li>
+            
+            <li><a href="resources/pdf_installer.exe"><i class="fa fa-download"></i> <span>PDF Installer</span>
+           </a></li>
 
       </ul>
 
